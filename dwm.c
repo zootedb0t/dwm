8c91b8ac389cef491c944ddcd017fd180bba1243
stoney <05kraken@proton.me>
2023-01-12 16:38:46 +0530

    remove viewontag patch shiftview modified

--- dwm.c
+++ dwm.c
@@ -2537,9 +2537,6 @@ tag(const Arg *arg)
 		selmon->sel->tags = arg->ui & TAGMASK;
 		focus(NULL);
 		arrange(selmon);
-		if (viewontag && ((arg->ui & TAGMASK) != TAGMASK)) {
-			view(arg);
-		}
 	}
 }
 
