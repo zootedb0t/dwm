3d2bae7e90b933cf91fe6883782ecc3b72b1b436
stoney <05kraken@proton.me>
2023-01-23 13:23:39 +0530

    closes #4

--- dwm.c
+++ dwm.c
@@ -1146,10 +1146,6 @@ drawbar(Monitor *m)
 
 	/* draw status first so it can be overdrawn by tags later */
 	if (m == selmon) { /* status is only drawn on selected monitor */
-		// drw_setscheme(drw, scheme[SchemeNorm]);
-		// tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
-		// drw_text(drw, m->ww - tw - stw, 0, tw, bh, 0, stext, 0);
-
 		tw = m->ww - drawstatusbar(m, bh, stext);
 	}
 
@@ -1176,7 +1172,10 @@ drawbar(Monitor *m)
 
 	if ((w = m->ww - tw - stw - x) > bh) {
 		if (m->sel) {
-			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
+			// Default highlight behaviour
+			// drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
+      // Now window title will get normal highlight
+			drw_setscheme(drw, scheme[SchemeNorm]);
 			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
 			if (m->sel->isfloating) {
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
