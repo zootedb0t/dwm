8b47edc24490a9f127d4f762b908ac7e8288f395
zootedb0t <05kraken@proton.me>
2023-04-18 08:12:22 +0530

    update layoutmenu.sh with new icons

--- scripts/layoutmenu.sh
+++ scripts/layoutmenu.sh
@@ -2,15 +2,15 @@
 
 # Use tabs to separate not space. If expandtab is set in vim. Disable it set expandtab!.
 cat <<EOF | xmenu
-[]= Tiled Layout	0
-[M] Monocle Layout	1
-[@] Spiral Layout	2
-[\\] Dwindle Layout	3
-D[] Deck Layout	4
-TTT Bstack Layout	5
---- Horizgrid Layout	6
-::: Gaplessgrid Layout	7
-|M| Centeredmaster Layout	8
->M> Centeredfloatingmaster Layout	9
-><> Floating Layout	10
+󰽙 Tiled Layout	0
+󰍉 Monocle Layout	1
+󱍸 Spiral Layout	2
+󰪏 Dwindle Layout	3
+󰘸 Deck Layout	4
+󱕕 Bstack Layout	5
+󰝘 Horizgrid Layout	6
+󱗼 Gaplessgrid Layout	7
+󱒅 Centeredmaster Layout	8
+󱒆 Centeredfloatingmaster Layout	9
+ Floating Layout	10
 EOF
