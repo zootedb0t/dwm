800a4360161d9cd1f0aaee3067f27a8245f98842
zootedb0t <05kraken@proton.me>
2024-04-02 08:48:45 +0530

    update dwm to version 6.5

--- config.def.h
+++ config.def.h
@@ -25,7 +25,7 @@ static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows sel
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray             = 1;   /* 0 means no systray */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:pixelsize=20:antialias=true:autohint=true",
+static const char *fonts[]          = { "JetBrains Mono:pixelsize=20:antialias=true:autohint=true",
                                         "JoyPixels:pixelsize=20:antialias=true:autohint=true",
                                       };
 static const char dmenufont[]       = "monospace:size=10";
